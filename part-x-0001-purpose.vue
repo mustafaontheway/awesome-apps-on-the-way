9f6a212a6defc2b90bc7c838c171a7529dd5011f
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-10-28 23:29:50 +0300

    Update part-x-0001-purpose.vue

--- part-x-0001-purpose.vue
+++ part-x-0001-purpose.vue
@@ -1,1 +1,16 @@
+<script setup>
+
+  let year = 2025
+
+  let purpose = "Stunning Web3 and AI Apss"
+
+</script>
+
+<template>
+  <div>{{ purpose }} in {{ year }}</div>
+</template>
+
+<style scoped>
+
+</style>
 
