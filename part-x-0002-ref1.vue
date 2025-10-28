27889582e3cf98cfa28b7e627b737db6a8331b4f
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-10-28 23:46:19 +0300

    Create part-x-0002-ref1.vue

---
+++ part-x-0002-ref1.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+  import {ref} from "vue"
+
+  let year = ref(2025)
+
+  function yearUp() {
+    year.value++
+  }
+
+</script>
+
+<template>
+  
+  <p>Year is {{ year }} </p>
+
+  <button @click="yearUp">Year ++</button>
+  
+</template>
+
+<style scoped>
+
+</style>
