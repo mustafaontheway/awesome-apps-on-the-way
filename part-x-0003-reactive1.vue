124edea09b970bc448270104f010176344724e1a
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-10-30 20:21:57 +0300

    Create part-x-0003-reactive1.vue

---
+++ part-x-0003-reactive1.vue
@@ -0,0 +1,29 @@
+<script setup>
+
+  import {reactive} from "vue"
+
+  const empData = reactive({
+    "name": "Ayhan Bilir",
+    "dep": "Marketing",
+    "salary": 42_000
+  })
+
+  function salaryChange() {
+    empData.salary += 5000
+  }
+
+</script>
+
+<template>
+  
+  <div>
+    Employee name: {{ empData.name }} - department: {{ empData.dep }} - salary: {{ empData.salary }} ₺
+  </div>
+
+  <button @click="salaryChange">Salary Increase (5000 ₺)</button>
+  
+</template>
+
+<style scoped>
+
+</style>
